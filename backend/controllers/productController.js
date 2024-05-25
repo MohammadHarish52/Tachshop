4f1542b0596301a402466983f00b04b688aa3036
tachyonok <tachyonparticle123@gmail.com>
2024-05-26 00:57:45 +0530

    review section

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -80,10 +80,47 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   create a new review
+// @route  DELETe /api/products/:id/reviews
+// @access Private/
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const isReviewed = product.reviews.find(
+      (rev) => rev.user.toString() === req.user._id.toString()
+    );
+    if (isReviewed) {
+      res.status(400);
+      throw new Error("product already reviewed");
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "review added" });
+  } else {
+    res.status(404);
+    throw new Error("resource not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
