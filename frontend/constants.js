5315bdbabc9edecf7253fbcfbf50f407f1ce2bdc
tachyonok <tachyonparticle123@gmail.com>
2024-06-15 20:44:51 +0530

    uri changes

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,8 +1,6 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === "development" ? "" : "";
 export const BASE_URL = "";
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
-export const UPLOADS_URL = "/api/upload";
+export const PRODUCTS_URL = "http://localhost:5000/api/products";
+export const USERS_URL = "http://localhost:5000/api/users";
+export const ORDERS_URL = "http://localhost:5000/api/orders";
+export const PAYPAL_URL = "http://localhost:5000/api/config/paypal";
+export const UPLOADS_URL = "http://localhost:5000/api/upload";
